8ad0f21029f4688ad59903ff5e6dfb2170cf70e1
swee <42469735+sweeloke@users.noreply.github.com>
2019-08-17 09:51:24 -0400

    Delete project_setup.py

--- util/project_setup.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-pd.set_option('display.max_columns', 10)
-
-class ProjectSetup:
-  home_dir = '/content/gdrive/My Drive/project_scs3253'
-  data_dir = f'{home_dir}/data'
-  raw_downloaded_dir = f'{data_dir}/raw_downloaded'
-  raw_tappy_dir = f'{raw_downloaded_dir}/Tappy Data'
-  raw_users_dir = f'{raw_downloaded_dir}/Archived users'
